aa19582bbea908b17eced5f99f85dc80003d9e8f
LeannF <leann.farant@outlook.fr>
2025-03-06 23:13:12 +0100

    bcryptjs

--- express_app/server/services/users.js
+++ express_app/server/services/users.js
@@ -1,5 +1,5 @@
 const User = require('../models/user');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 exports.getByEmail = async(req, res) => {
     const email = req.params.email
